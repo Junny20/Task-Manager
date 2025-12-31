525e7508b2bc0ea880b279a4d543d79b3bf4fa1a
Junnian Liu <liuj4467@Junnians-MacBook-Air.local>
2025-12-31 14:24:04 -0800

    added color and dotted lines

--- src/app.rs
+++ src/app.rs
@@ -3,6 +3,7 @@ use std::{sync::mpsc::Receiver, time::Duration, collections::VecDeque};
 use crate::cpusnapshot::CpuSnapshot;
 
 const ROLLING_GRAPH_HEIGHT_PX: f32 = 80.0;
+const CORES_UPPER_LIMIT: usize = 20;
 
 pub struct SystemMonitorApp {
     receiver: Receiver<CpuSnapshot>,
@@ -16,8 +17,8 @@ impl SystemMonitorApp {
         Self {
             receiver,
             latest_snapshot: None,
-            overall_cpu_history: VecDeque::new(),
             per_core_cpu_history: None,
+            overall_cpu_history: VecDeque::with_capacity(CORES_UPPER_LIMIT),
         }
     }
 }
@@ -34,10 +35,13 @@ impl eframe::App for SystemMonitorApp {
 
                 if let None = &mut self.per_core_cpu_history {
                     let n: usize = cpu_snapshot.per_core_cpu_usage.len();
+
+                    // implements the with_capacity method 
                     self.per_core_cpu_history = Some(vec![VecDeque::new(); n]);
                 }
 
                 let per_core_cpu_history: &mut Vec<VecDeque<f32>> = self.per_core_cpu_history.as_mut().unwrap();
+
                 for (index, value) in cpu_snapshot.per_core_cpu_usage.iter().enumerate() {
                     let per_core_values: &mut VecDeque<f32> = &mut per_core_cpu_history[index];
                     per_core_values.push_back(*value);
@@ -87,7 +91,7 @@ impl eframe::App for SystemMonitorApp {
                 });
 
                 ui.group(|ui| {
-                    ui.label("Rolling graph overall cpu history");
+                    ui.label("Overall cpu history");
                     // allocates a rectangle size in form of length width vector
                     let desired_size: Vec2 = vec2(ui.available_width(), ROLLING_GRAPH_HEIGHT_PX);
                     // creates rectangle in ui
@@ -96,8 +100,9 @@ impl eframe::App for SystemMonitorApp {
                     draw_graph(&rect, ui, &self.overall_cpu_history);
                 });
 
-                for core in self.per_core_cpu_history.as_mut().unwrap() {
+                for (index, core) in self.per_core_cpu_history.as_mut().unwrap().iter().enumerate() {
                     ui.group(|ui| {
+                        ui.label(format!("Core {} cpu history", index + 1));
                         let desired_size: Vec2 = vec2(ui.available_width(), ROLLING_GRAPH_HEIGHT_PX);
                         let (rect, _response): (Rect, Response) = 
                             ui.allocate_exact_size(desired_size, Sense::hover());
@@ -127,15 +132,46 @@ fn draw_graph(rect: &Rect, ui: &mut Ui, history: &VecDeque<f32>) {
         StrokeKind::Outside,
     );
 
+    // draws 50% line
+    let half: f32 = rect.bottom() - ((rect.bottom() - rect.top()) / 2.0);
+    draw_dotted_hline(&rect, half, &painter);
+
     let n: usize = history.len();
     let points: Vec<Pos2> = history.iter().enumerate().map(|(index, value)| make_point(index, value, n, rect)).collect();
-    for segment in points.windows(2) { // magic num
+    for (index, segment) in points.windows(2).enumerate() { // magic num
+        let color: Color32;
+        let stroke_width: f32;
+        match index {
+            9 => {
+                let value: f32 = history[index];
+                color = find_color(value);
+                stroke_width = find_stroke_width(value)
+            },
+            _ => {
+                let value: f32 = (history[index] + history[index + 1]) / 2.0;
+                color = find_color(value);
+                stroke_width = find_stroke_width(value)
+            }
+        }
         painter.line_segment(
             [segment[0], segment[1]], 
-            Stroke::new(2.0, Color32::LIGHT_GREEN)
+            Stroke::new(stroke_width, color)
         );
     }
 
+}
+
+fn draw_dotted_hline(rect: &Rect, y: f32, painter: &Painter) {
+    let mut dotted: Vec<Rangef> = Vec::new();
+    let mut start: f32 = rect.left();
+    let end: f32 = rect.right();
+    while start < end {
+        dotted.push(Rangef { min: start, max: start + 2.5 });
+        start += 5.0;
+    }
+    for range in dotted.into_iter() {
+        painter.hline(range, y, Stroke::new(1.0, Color32::GRAY));
+    }
 }
 
 fn make_point(index: usize, value: &f32, n: usize, rect: &Rect) -> Pos2 {
@@ -145,3 +181,19 @@ fn make_point(index: usize, value: &f32, n: usize, rect: &Rect) -> Pos2 {
     let y: f32 = plot_rect.bottom() - (value / 100.0) * plot_rect_height; // magic num
     Pos2 { x, y }
 }
+
+fn find_color(value: f32) -> Color32 {
+    match value {
+        value if value < 50.0 => Color32::LIGHT_GREEN,
+        value if value < 80.0 => Color32::YELLOW,
+        _ => Color32::RED,
+    }
+}
+
+fn find_stroke_width(value: f32) -> f32 {
+    match value {
+        value if value < 50.0 => 2.0,
+        value if value < 80.0 => 2.5,
+        _ => 3.0,
+    }
+} // magic nums
